174d677e745040c5d2de945c4e9622319957c157
topkst <45350454+topkst@users.noreply.github.com>
2021-08-27 18:39:22 +0900

    08-27 커밋

---
+++ app/src/main/java/kst/app/fcuseddeal/DBKey.kt
@@ -0,0 +1,7 @@
+package kst.app.fcuseddeal
+
+class DBKey {
+    companion object{
+        const val DB_ARTICLES = "Articles"
+    }
+}
--- app/src/main/java/kst/app/fcuseddeal/home/HomeFragment.kt
+++ app/src/main/java/kst/app/fcuseddeal/home/HomeFragment.kt
@@ -1,7 +1,91 @@
 package kst.app.fcuseddeal.home
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kst.app.fcuseddeal.DBKey.Companion.DB_ARTICLES
 import kst.app.fcuseddeal.R
+import kst.app.fcuseddeal.databinding.FragmentHomeBinding
 
-class HomeFragment:Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var binding: FragmentHomeBinding? = null
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
+    private lateinit var articleDB: DatabaseReference
+    private lateinit var articleAdapter: ArticleAdapter
+
+    private val articleList = mutableListOf<ArticleModel>()
+    private val listener = object : ChildEventListener{
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            val articleModel = snapshot.getValue(ArticleModel::class.java)
+            Log.d("gwan2103","$articleModel")
+
+            articleModel ?: return
+
+            articleList.add(articleModel)
+            articleAdapter.submitList(articleList)
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onCancelled(error: DatabaseError) {
+        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+        articleList.clear()
+        articleDB = Firebase.database.reference.child(DB_ARTICLES)
+        articleAdapter = ArticleAdapter()
+
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+
+        fragmentHomeBinding.appFloatingBt.setOnClickListener {
+            context?.let {
+//                if (auth.currentUser != null){
+                    val intnet = Intent(it,AddArticleActivity::class.java)
+                    startActivity(intnet)
+//                } else {
+//                    Snackbar.make(view,"로그인 후 사용해주세요",Snackbar.LENGTH_LONG).show()
+//               }
+
+
+            }
+        }
+
+        articleDB.addChildEventListener(listener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        articleAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        articleDB.removeEventListener(listener)
+    }
 }
